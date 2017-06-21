b548935eca04ceb5bd8857079de2e96b51da9f6b
Igor Cheledinov <cheledinov@gmail.com>
2017-06-21 13:20:25 +0300

    Enable redis caching.

--- sites/default/settings.pantheon.php
+++ sites/default/settings.pantheon.php
@@ -197,3 +197,24 @@ if (empty($settings['file_scan_ignore_directories'])) {
   ];
 }
 
+// Configure Redis
+
+if (defined('PANTHEON_ENVIRONMENT')) {
+  // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
+  $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+
+  //phpredis is built into the Pantheon application container.
+  $settings['redis.connection']['interface'] = 'PhpRedis';
+  // These are dynamic variables handled by Pantheon.
+  $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
+  $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
+  $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
+
+  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
+  $settings['cache_prefix']['default'] = 'pantheon-redis';
+
+  // Always set the fast backend for bootstrap, discover and config, otherwise this gets lost when redis is enabled.
+  $settings['cache']['bins']['bootstrap'] = 'cache.backend.chainedfast';
+  $settings['cache']['bins']['discovery'] = 'cache.backend.chainedfast';
+  $settings['cache']['bins']['config']    = 'cache.backend.chainedfast';
+}
